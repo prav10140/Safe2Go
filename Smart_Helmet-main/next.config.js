92310a47dda2ce7bde29df20b75820c48601b501
PRAVEEN PATEL <prav9406@gmail.com>
2025-10-02 14:49:32 +0530

    Add files via upload

---
+++ Smart_Helmet-main/next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  output: "standalone",
+};
+
+module.exports = nextConfig;
